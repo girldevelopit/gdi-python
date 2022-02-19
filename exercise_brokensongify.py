66a8a26f0f99ed3c0b18f9388a6c311a051d179a
Pamela Fox <pamela.fox@gmail.com>
2022-02-18 21:14:58 -0800

    Create exercise_brokensongify.py

---
+++ exercise_brokensongify.py
@@ -0,0 +1,23 @@
+# Broken #1
+def songify(lyric)
+  return "🎼" + lyric + "🎶"
+
+songify("siiiinnnging in the rain")
+
+# Broken #2
+def songify(lyric):
+  return "🎼" + lyric "🎶"
+
+songify("siiiinnnging in the rain")
+
+# Broken #3
+def songify(lyric):
+  return "🎼" + lyric + "🎶"
+
+songify("siiiinnnging in the rain"))
+
+# Broken #4
+def songify(lyric):
+  return "🎼" + lyric + "🎶"
+
+print(songify("siiiinnnging in the rain")
